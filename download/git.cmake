75df96b2baaa10389ddffc4d385f00dd5fb42e99
INJAE <8687lee@gmail.com>
2019-02-06 14:38:25 +0900

    update git tool

---
+++ download/git.cmake
@@ -0,0 +1,57 @@
+find_package(Git REQUIRED)
+if(NOT GIT_FOUND)
+    message(FATAL_ERROR "git not found!")
+endif()
+
+function(git_clone)
+    cmake_parse_arguments(ARG "QUIET" "URL;BRANCH;PATH" "" ${ARGN})
+    list(LENGTH ARG_UNPARSED_ARGUMENTS size)
+    if(${size} < 1)
+        message(FATAL_ERROR "You must provide a name")
+    endif()
+    list(GET ARG_UNPARSED_ARGUMENTS 0 name)
+
+    if(NOT ARG_URL)
+        message(FATAL_ERROR "You must provide a git url")
+    endif()
+
+    if(NOT ARG_BRANCH)
+        set(ARG_BRANCH master)
+    endif()
+
+    if(NOT ARG_PATH)
+        set(ARG_PATH ${CMAKE_CURRENT_SOURCE_DIR})
+    endif()
+
+    if(EXIST ${ARG_PATH}/${name})
+        execute_process(
+            COMMAND ${GIT_EXECUTALBE} fetch -all
+            COMMAND ${GIT_EXECUTALBE} reset --hard origin/${ARG_BRANCH}
+            COMMAND ${GIT_EXECUTALBE} fetch pull origin ${ARG_BRANCH}
+            WORKING_DIRECTORY ${ARG_PATH}
+            OUTPUT_VALUE output
+            )
+    else()
+        execute_process(
+            COMMAND ${GIT_EXECUTALBE} clone ${ARG_URL} --recursive
+            WORKING_DIRECTORY ${ARG_PATH}
+            OUTPUT_VALUE output
+            )
+    endif()
+
+    if(NOT ARG_QUIET)
+        message(STATUS "[cppm] ${output}")
+    endif()
+
+    if(NOT ${ARG_BRANCH} MATCHES "master")
+        excute_process(
+            COMMAND ${GIT_EXCUTEABLE} checkout ${ARG_BRANCH}
+            WORKING_DIRECTORY ${ARG_PATH}
+            OUTPUT_VALUE output
+            )
+    endif()
+
+    if(NOT ARG_QUIET)
+        message(STATUS "[cppm] ${output}")
+    endif()
+endfunction()
