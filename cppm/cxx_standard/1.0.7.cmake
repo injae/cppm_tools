9b4ecccfcaf9071a266ce310b5e14fbf3bc47f8f
injae <8687lee@gmail.com>
2019-04-10 19:16:42 +0900

    msvc compiler option bug fix

--- cppm/cxx_standard/1.0.7.cmake
+++ cppm/cxx_standard/1.0.7.cmake
@@ -2,4 +2,9 @@ macro(_cppm_cxx_standard _version)
     set(CMAKE_CXX_STANDARD ${_version})
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
     set(CMAKE_CXX_EXTENSIONS OFF)
+
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++${_version}")
+    endif()
+
 endmacro()
