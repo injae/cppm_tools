c59b22c8d0e04f298996824d81e5db27239fb8b3
injae <8687lee@gmail.com>
2019-09-28 18:24:11 +0900

    cppm new value no_message

--- cppm/cppm_project.cmake
+++ cppm/cppm_project.cmake
@@ -1,3 +1,7 @@
 macro(_cppm_project)
     cmake_parse_arguments(ARG "" "" "" ${ARGN})
+    if(NOT ${CMAKE_PROJECT_NAME} MATCHES ${PROJECT_NAME})
+        set(SUB_PROJECT TRUE)
+        set(NO_MESSAGE TRUE)
+    endif()
 endmacro()
