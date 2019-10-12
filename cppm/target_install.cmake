080854778db5da2532b256c97f294fb520d9db4c
injae <8687lee@gmail.com>
2019-10-12 21:51:31 +0900

    cppm target_install changed

--- cppm/target_install.cmake
+++ cppm/target_install.cmake
@@ -5,30 +5,34 @@ macro(_cppm_target_install)
         install(TARGETS ${name} RUNTIME DESTINATION bin)
     endif()
     if(${${name}_target_type} MATCHES "LIBRARY")
-        install(TARGETS ${name} EXPORT ${CMAKE_PROJECT_NAME}-targets
-            ARCHIVE DESTINATION lib 
-            LIBRARY DESTINATION lib
-            RUNTIME DESTINATION bin
-        )
-        install(DIRECTORY include/ DESTINATION include)
-
-        install(EXPORT ${CMAKE_PROJECT_NAME}-targets
-            FILE ${CMAKE_PROJECT_NAME}-config.cmake
-            NAMESPACE ${CMAKE_PROJECT_NAME}::
-            DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
-        )
-        # project-config-version install part
+        # project-config-version.cmake install part
         include(CMakePackageConfigHelpers)
         write_basic_package_version_file(
           ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
           VERSION ${${CMAKE_PROJECT_NAME}_VERSION}
           COMPATIBILITY ExactVersion
         ) 
-
         install(FILES
           ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
           DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
         )
+        # project-targets.cmake install part
+        install(TARGETS ${name} EXPORT ${CMAKE_PROJECT_NAME}-targets
+            ARCHIVE  DESTINATION lib 
+            LIBRARY  DESTINATION lib
+            RUNTIME  DESTINATION bin
+            INCLUDES DESTINATION include
+        )
+        install(DIRECTORY include/ DESTINATION include)
+
+        install(EXPORT ${CMAKE_PROJECT_NAME}-targets
+            FILE ${CMAKE_PROJECT_NAME}-targets.cmake
+            NAMESPACE ${CMAKE_PROJECT_NAME}::
+            DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
+        )
+
+        #set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
+        #export(PACKAGE ${name})
         if(SUB_PROJECT)
             message(STATUS "[cppm] Find Package: ${name}/${PROJECT_VERSION}")
         endif()
