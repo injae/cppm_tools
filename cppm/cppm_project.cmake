4a54c0b2074220f127948e155c40b43bc48c4185
injae <8687lee@gmail.com>
2020-02-12 19:59:13 +0900

    test

--- cppm/cppm_project.cmake
+++ cppm/cppm_project.cmake
@@ -1,9 +1,19 @@
 macro(cppm_project)
     cmake_parse_arguments(ARG "NIGHTLY" "" "" ${ARGN})
-    FetchContent_Populate(cmake-tools-${CPPM_VERSION}
-        GIT_REPOSITORY https://github.com/injae/cppm_tools.git
-        GIT_TAG        ${CPPM_VERSION}
-        SOURCE_DIR     ${CPPM_CORE}
-        QUIET
-    )
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/cppm-tools/download.cmake.in
+"cmake_minimum_required(VERSION 3.6)
+project(cpp-tool-${CPPM_VERSION}-install NONE)
+include(ExternalProject)
+ExternalProject_Add(cppm-tools-${CPPM_VERSION}
+    GIT_REPOSITORY https://github.com/injae/cppm_tools.git
+    GIT_TAG ${CPPM_VERSION}
+    SOURCE_DIR ${CPPM_CORE}
+    BINARY_DIR ${CPPM_CACHE}/cppm_tools/1.0.9/build
+    #QUIET
+)"
+)
+    configure_file(${CMAKE_CURRENT_BINARY_DIR}/cppm-tools/download.cmake.in
+                ${CMAKE_CURRENT_BINARY_DIR}/cppm-tools/CMakeLists.txt)
+    execute_process(COMMAND "${CMAKE_COMMAND} ." WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cppm-tools)
+    execute_process(COMMAND "${CMAKE_COMMAND} --build ." WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cppm-tools)
 endmacro()
