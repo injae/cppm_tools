69358d17bbc37263cf01e4edd94c1bbc96f91b2d
injae <8687lee@gmail.com>
2020-02-21 16:44:20 +0900

    cppm load more fast

--- cppm/cppm_project.cmake
+++ cppm/cppm_project.cmake
@@ -1,7 +1,9 @@
 macro(cppm_project)
     cmake_parse_arguments(ARG "NIGHTLY" "" "" ${ARGN})
-    cppm_download_package(cppm-tools
-        GIT https://github.com/injae/cppm_tools.git
-        GIT_TAG ${CPPM_VERSION}
-    )
+    if(NOT IS_CPPM_LOADED)
+        cppm_download_package(cppm-tools
+            GIT https://github.com/injae/cppm_tools.git
+            GIT_TAG ${CPPM_VERSION}
+        )
+    endif()
 endmacro()
