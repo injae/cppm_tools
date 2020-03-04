58598d8b094b2a6d4fa42935cd89b22553c764b6
injae <8687lee@gmail.com>
2020-03-04 23:52:28 +0900

    new build optional flag

--- cppm/target_define.cmake
+++ cppm/target_define.cmake
@@ -1,50 +1,58 @@
 macro(cppm_target_define)
-    cmake_parse_arguments(ARG "BINARY;STATIC;SHARED;INTERFACE" "" "SOURCES" ${ARGN})
+    cmake_parse_arguments(ARG "BINARY;STATIC;SHARED;INTERFACE" "OPTIONAL" "SOURCES" ${ARGN})
     list(GET ARG_UNPARSED_ARGUMENTS 0 name)
 
     set(lib_include_dir "include")
     set(lib_source_dir  "src")
 
-    if(${ARG_BINARY})
-        add_executable(${name} "")
-        set(${name}_target_type "BINARY")
-        target_include_directories(${name}
-            PUBLIC  ${CMAKE_CURRENT_SOURCE_DIR}/include
-            PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/src
-        )
-    
-    elseif(${ARG_STATIC} OR ${ARG_SHARED})
-        set(${name}_target_type "LIBRARY")
-        if(${ARG_STATIC})
-            add_library(${name} STATIC "")
-        elseif(${ARG_SHARED})
-            add_library(${name} SHARED "")
+    set(_O_${name} ${CMAKE_PROJECT_NAME}_${name})
+    if(ARG_OPTIONAL)
+        set(${_O_${name}} ${ARG_OPTIONAL})
+    else()
+        set(${_O_${name}} TRUE)
+    endif()
+    if(${_O_${name}})
+        if(${ARG_BINARY})
+            add_executable(${name} "")
+            set(${name}_target_type "BINARY")
+            target_include_directories(${name}
+                PUBLIC  ${CMAKE_CURRENT_SOURCE_DIR}/include
+                PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/src
+            )
+
+        elseif(${ARG_STATIC} OR ${ARG_SHARED})
+            set(${name}_target_type "LIBRARY")
+            if(${ARG_STATIC})
+                add_library(${name} STATIC "")
+            elseif(${ARG_SHARED})
+                add_library(${name} SHARED "")
+            endif()
+            add_library(${PROJECT_NAME}::${name} ALIAS ${name})
+            set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
+            set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
+            target_include_directories(${name}
+                PUBLIC
+                    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${lib_include_dir}>
+                    $<INSTALL_INTERFACE:include>
+                PRIVATE
+                    ${CMAKE_CURRENT_SOURCE_DIR}/${lib_source_dir}
+            )
+        elseif(${ARG_INTERFACE})
+            set(${name}_target_type "LIBRARY")
+            add_library(${name} INTERFACE)
+            add_library(${PROJECT_NAME}::${name} ALIAS ${name})
+            target_include_directories(${name}
+                INTERFACE
+                    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${lib_include_dir}>
+                    $<INSTALL_INTERFACE:include>
+            )
         endif()
-        add_library(${PROJECT_NAME}::${name} ALIAS ${name})
-        set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
-        set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
-        target_include_directories(${name}
-            PUBLIC
-                $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${lib_include_dir}>
-                $<INSTALL_INTERFACE:include>
-            PRIVATE
-                ${CMAKE_CURRENT_SOURCE_DIR}/${lib_source_dir}
-        )
-    elseif(${ARG_INTERFACE})
-        set(${name}_target_type "LIBRARY")
-        add_library(${name} INTERFACE)
-        add_library(${PROJECT_NAME}::${name} ALIAS ${name})
-        target_include_directories(${name}
-            INTERFACE
-                $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${lib_include_dir}>
-                $<INSTALL_INTERFACE:include>
-        )
-    endif()
-    if(SUB_PROJECT)
-        include_directories(${lib_include_dir})
-    endif()
-    target_compile_features(${name} INTERFACE "cxx_std_${cxx_standard}")
-    if(DEFINED ARG_SOURCES)
-        target_sources(${name} PRIVATE ${ARG_SOURCES})
+        if(SUB_PROJECT)
+            include_directories(${lib_include_dir})
+        endif()
+        target_compile_features(${name} INTERFACE "cxx_std_${cxx_standard}")
+        if(DEFINED ARG_SOURCES)
+            target_sources(${name} PRIVATE ${ARG_SOURCES})
+        endif()
     endif()
 endmacro()
--- cppm/target_install.cmake
+++ cppm/target_install.cmake
@@ -15,52 +15,52 @@ endfunction()
 macro(cppm_target_install)
     cmake_parse_arguments(ARG "" "" "" ${ARGN})
     list(GET ARG_UNPARSED_ARGUMENTS 0 name)
-    if(${${name}_target_type} MATCHES "BINARY")
-        install(TARGETS ${name} RUNTIME DESTINATION bin)
-    endif()
-    if(${${name}_target_type} MATCHES "LIBRARY")
-        # project-config-version.cmake install part
-        include(CMakePackageConfigHelpers)
-        write_basic_package_version_file(
-          ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
-          VERSION ${${CMAKE_PROJECT_NAME}_VERSION}
-          COMPATIBILITY ExactVersion
-        ) 
-        install(FILES
-          ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
-          DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
-        )
-        cppm_write_target_dependency_file(${name})
-
-        #file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake
-        #    "include(\$\{CMAKE_CURRENT_LIST_DIR\}/${CMAKE_PROJECT_NAME}-targets.cmake)\n"
-        #)
-        #configure_file("${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake.in"
-        #               "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake")
-        install(FILES
-            ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake
-            DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
-        )
-
-        # project-targets.cmake install part
-        install(TARGETS ${name} EXPORT ${CMAKE_PROJECT_NAME}-targets
-            ARCHIVE  DESTINATION lib 
-            LIBRARY  DESTINATION lib
-            RUNTIME  DESTINATION bin
-        )
-        install(DIRECTORY include/ DESTINATION include)
-
-        install(EXPORT ${CMAKE_PROJECT_NAME}-targets
-            FILE ${CMAKE_PROJECT_NAME}-targets.cmake
-            NAMESPACE ${CMAKE_PROJECT_NAME}::
-            DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
-        )
-
-        #set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
-        #export(PACKAGE ${name})
-        if(SUB_PROJECT)
-            message(STATUS "[cppm] Find Package: ${name}/${PROJECT_VERSION}")
+    if(${_O_${name}})
+        if(${${name}_target_type} MATCHES "BINARY")
+            install(TARGETS ${name} RUNTIME DESTINATION bin)
         endif()
-        message(STATUS "[cppm] Module : ${CMAKE_PROJECT_NAME}::${name}")
+        if(${${name}_target_type} MATCHES "LIBRARY")
+            # project-config-version.cmake install part
+            include(CMakePackageConfigHelpers)
+            write_basic_package_version_file(
+            ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
+            VERSION ${${CMAKE_PROJECT_NAME}_VERSION}
+            COMPATIBILITY ExactVersion
+            ) 
+            install(FILES
+            ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
+            DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
+            )
+            cppm_write_target_dependency_file(${name})
+
+            #file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake
+            #    "include(\$\{CMAKE_CURRENT_LIST_DIR\}/${CMAKE_PROJECT_NAME}-targets.cmake)\n"
+            #)
+            #configure_file("${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake.in"
+            #               "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake")
+            install(FILES
+                ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config.cmake
+                DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
+            )
+
+            # project-targets.cmake install part
+            install(TARGETS ${name} EXPORT ${CMAKE_PROJECT_NAME}-targets
+                ARCHIVE  DESTINATION lib 
+                LIBRARY  DESTINATION lib
+                RUNTIME  DESTINATION bin
+            )
+            install(DIRECTORY include/ DESTINATION include)
+
+            install(EXPORT ${CMAKE_PROJECT_NAME}-targets
+                FILE ${CMAKE_PROJECT_NAME}-targets.cmake
+                NAMESPACE ${CMAKE_PROJECT_NAME}::
+                DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
+            )
+
+            if(SUB_PROJECT)
+                message(STATUS "[cppm] Find Package: ${name}/${PROJECT_VERSION}")
+            endif()
+            message(STATUS "[cppm] Module : ${CMAKE_PROJECT_NAME}::${name}")
+        endif()
     endif()
 endmacro()
