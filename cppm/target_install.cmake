a05a576feb248a756f044f1edb89df1753ddd80c
injae <8687lee@gmail.com>
2020-03-18 21:12:47 +0900

    now can set custom namespace

--- cppm/target_install.cmake
+++ cppm/target_install.cmake
@@ -16,17 +16,17 @@ endfunction()
 macro(cppm_target_install)
     cmake_parse_arguments(ARG "" "" "" ${ARGN})
     list(GET ARG_UNPARSED_ARGUMENTS 0 name)
-    if(${_O_${name}})
-        if(${${name}_target_type} MATCHES "BINARY")
-            install(TARGETS ${name} RUNTIME DESTINATION bin)
-                                                      # $HOME/.cppm/local/share/${name}-${version}
+    if(TARGET ${name})
+        get_target_property(_target_type ${name} CPPM_TYPE)
+        if(_target_type MATCHES "BINARY")
+            install(TARGETS ${name} RUNTIME DESTINATION bin) # $HOME/.cppm/local/share/${name}-${version}
             get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_INSTALL_PREFIX}/../../" ABSOLUTE)
             if(PACKAGE_PREFIX_DIR STREQUAL "${CPPM_PREFIX}")
                 install(DIRECTORY "${CMAKE_INSTALL_PREFIX}/bin/" DESTINATION ${CPPM_PREFIX}/bin USE_SOURCE_PERMISSIONS)
             endif()
         endif()
-        if(${${name}_target_type} MATCHES "LIBRARY")
-            # project-config-version.cmake install part
+        if(_target_type MATCHES "LIBRARY")
+            get_target_property(_namespace ${name} CPPM_NAMESPACE)
             include(CMakePackageConfigHelpers)
             write_basic_package_version_file(
             ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}-config-version.cmake
@@ -54,14 +54,14 @@ macro(cppm_target_install)
 
             install(EXPORT ${CMAKE_PROJECT_NAME}-targets
                 FILE ${CMAKE_PROJECT_NAME}-targets.cmake
-                NAMESPACE ${CMAKE_PROJECT_NAME}::
+                NAMESPACE ${_namespace}::
                 DESTINATION lib/cmake/${CMAKE_PROJECT_NAME}
             )
 
             if(SUB_PROJECT)
                 message(STATUS "[cppm] Find Package: ${name}/${PROJECT_VERSION}")
             endif()
-            message(STATUS "[cppm] Module : ${CMAKE_PROJECT_NAME}::${name}")
+            message(STATUS "[cppm] Module : ${_namespace}::${name}")
         endif()
     endif()
 endmacro()
