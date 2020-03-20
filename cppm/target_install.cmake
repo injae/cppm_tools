f3cec47b4cc9aeebb63a219cfbc610f8e58e9440
injae <8687lee@gmail.com>
2020-03-20 21:43:22 +0900

    test

--- cppm/target_install.cmake
+++ cppm/target_install.cmake
@@ -23,7 +23,7 @@ macro(cppm_target_install)
             get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_INSTALL_PREFIX}/../../" ABSOLUTE)
             if(PACKAGE_PREFIX_DIR STREQUAL "${CPPM_PREFIX}")
                 add_custom_command(TARGET ${name} COMMAND
-                    ${CMAKE_COMMAND} -E create_symlink "${CMAKE_INSTALL_PREFIX}/bin/$<TARGET_FILE_NAME:${name}>" "${CPPM_PREFIX}/bin/$<TARGET_FILE_NAME:${name}>"  COMMENT "make symblic link")
+                    ${CMAKE_COMMAND} -E create_symlink "$<TARGET_FILE_NAME:${name}>" "${CPPM_PREFIX}/bin/$<TARGET_FILE_BASE_NAME:${name}>" COMMENT "make symblic link")
                 #install(DIRECTORY "${CMAKE_INSTALL_PREFIX}/bin/" DESTINATION ${CPPM_PREFIX}/bin USE_SOURCE_PERMISSIONS)
             endif()
         endif()
