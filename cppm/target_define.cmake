b49272c0bf2651cebd195fcfe3aeb14202f61376
injae <8687lee@gmail.com>
2020-04-24 20:31:53 +0900

    test

--- cppm/target_define.cmake
+++ cppm/target_define.cmake
@@ -65,7 +65,7 @@ endfunction()
 
 function(cppm_with_unit_test)
     string(TOUPPER ${CMAKE_PROJECT_NAME} upper_name) 
-    cmake_dependent_option("${upper_name}_BUILD_TESTING" "${CMAKE_PROJECT_NAME} build test"
+    cmake_dependent_option(${upper_name}_BUILD_TESTING "${CMAKE_PROJECT_NAME} build test"
         ON "CMAKE_BUILD_TYPE STREQUAL \"Debug\"" OFF)
     if(${${upper_name}_BUILD_TESTING})
         message("==>Unit Test<==")
