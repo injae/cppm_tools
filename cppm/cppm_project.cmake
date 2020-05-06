09d8fd41d9b6ff897dfefeecde4204605006ec4a
injae <8687lee@gmail.com>
2020-05-06 20:34:17 +0900

    now cppm can't default load vcpkg
    need cppm_project(WITH_VCPKG)

--- cppm/cppm_project.cmake
+++ cppm/cppm_project.cmake
@@ -1,12 +1,14 @@
 macro(cppm_project)
-    cmake_parse_arguments(ARG "NIGHTLY" "" "" ${ARGN})
+    cmake_parse_arguments(ARG "NIGHTLY;WITH_VCPKG" "" "" ${ARGN})
     if(NOT IS_CPPM_LOADED)
         cppm_download_package(cppm-tools
             GIT https://github.com/injae/cppm_tools.git
             GIT_TAG ${CPPM_VERSION}
         )
     endif()
-    _include_vcpkg()
+    if(ARG_WITH_VCPKG)
+        _include_vcpkg()
+    endif()
 endmacro()
 
 macro(_include_vcpkg)
