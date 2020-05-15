907c3aad2711053ca0aff24596998b803916ab88
injae <8687lee@gmail.com>
2020-05-15 21:57:19 +0900

    test

--- set_cppm_install_prefix.cmake
+++ set_cppm_install_prefix.cmake
@@ -1,5 +1,4 @@
 message("---------------install with cppm prefix")
 if(USE_CPPM_PATH)
-    include(utility/set_cmake_cache)
-    set_cache(CMAKE_INSTALL_PREFIX "${CPPM_PKGS}/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}" STRING)
+    set(CMAKE_INSTALL_PREFIX "${CPPM_PKGS}/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}" CACHE STRING)
 endif()
