5129eccb0e01faf311811ae68e0d5bf353caad58
injae <8687lee@gmail.com>
2020-05-18 23:23:45 +0900

    cppm_link now add install file path in install_manifest.txt

--- add_cppm_target.cmake
+++ add_cppm_target.cmake
@@ -5,6 +5,7 @@ set(cppm_symlink_file "${CMAKE_BINARY_DIR}/cppm_symlink.cmake")
 file(WRITE ${cppm_symlink_file}
 "include(${CPPM_CORE}/cppm/create_symlink.cmake)
 set(CPPM_ROOT \"${CPPM_ROOT}\")
+set(CPPM_TARGET_BINARY_DIR \"${CMAKE_BINARY_DIR}\")
 cppm_create_symlink(${CMAKE_INSTALL_PREFIX})
 ")
 add_custom_target(cppm_link COMMAND "${CMAKE_COMMAND}" -P "${cppm_symlink_file}")
