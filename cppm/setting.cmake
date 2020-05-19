273532af2941b22d3c3b5be65c09b0afcd969321
injae <8687lee@gmail.com>
2020-05-19 23:23:05 +0900

    now cppm_download use cmake option -DUSE_CPPM_PATH

--- cppm/setting.cmake
+++ cppm/setting.cmake
@@ -208,7 +208,6 @@ function(triplet_check triplet)
     endif()
     list(GET atoms 0 arch)
     list(GET atoms 1 platform)
-    message("-------- ${arch}-${platform}-${generator}")
 
     if(NOT "${arch}" STREQUAL "${cppm_target_arch}")
         set(_result FALSE PARENT_SCOPE)
