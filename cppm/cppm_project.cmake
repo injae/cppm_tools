74415e73d032197898611d849325f31d51285485
injae <8687lee@gmail.com>
2020-05-26 20:40:25 +0900

    remove regacy code

--- cppm/cppm_project.cmake
+++ cppm/cppm_project.cmake
@@ -1,12 +1,5 @@
 macro(cppm_project)
     cmake_parse_arguments(ARG "NIGHTLY;WITH_VCPKG" "" "" ${ARGN})
-    #if(NOT IS_CPPM_LOADED)
-    #    cppm_download_package(cppm-tools
-    #        GIT https://github.com/injae/cppm_tools.git
-    #        GIT_TAG ${CPPM_TOOLS_VERSION}
-    #        PATH  ${CPPM_CPPKG}/cppm-tools-${CPPM_TOOLS_VERSION}
-    #    )
-    #endif()
     if(ARG_WITH_VCPKG)
         set(WITH_VCPKG ON)
     endif()
