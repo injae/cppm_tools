5371a8746a559b7c34300c1aff77c117310f0123
injae <8687lee@gmail.com>
2020-10-10 20:26:55 +0900

    uinstall target update

--- cppm/uninstall.cmake
+++ cppm/uninstall.cmake
@@ -4,7 +4,7 @@
 #
 # Add the "uninstall" target for your project::
 #
-#   include(AddUninstallTarget)
+#   include(uninstall)
 #
 #
 # will create a file cmake_uninstall.cmake in the build directory and add a
@@ -35,34 +35,34 @@ endif()
 if(TARGET uninstall OR (TARGET UNINSTALL))
     message("this scope")
     return()
-endif()
+else()
+    set(_filename ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake)
 
-set(_filename ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake)
-
-file(WRITE ${_filename}
-"if(NOT EXISTS \"${CMAKE_BINARY_DIR}/install_manifest.txt\")
-    message(WARNING \"Cannot find install manifest: \\\"${CMAKE_BINARY_DIR}/install_manifest.txt\\\"\")
-    return()
-endif()
-file(READ \"${CMAKE_BINARY_DIR}/install_manifest.txt\" files)
-string(STRIP \"\${files}\" files)
-string(REGEX REPLACE \"\\n\" \";\" files \"\${files}\")
-list(REVERSE files)
-foreach(file \${files})
-    message(STATUS \"Uninstalling: \$ENV{DESTDIR}\${file}\")
-    if(EXISTS \"\$ENV{DESTDIR}\${file}\")
-        execute_process(
-            COMMAND \${CMAKE_COMMAND} -E remove \"\$ENV{DESTDIR}\${file}\"
-            OUTPUT_VARIABLE rm_out
-            RESULT_VARIABLE rm_retval)
-        if(NOT \"\${rm_retval}\" EQUAL 0)
-            message(FATAL_ERROR \"Problem when removing \\\"\$ENV{DESTDIR}\${file}\\\"\")
-        endif()
-    else()
-        message(STATUS \"File \\\"\$ENV{DESTDIR}\${file}\\\" does not exist.\")
+    file(WRITE ${_filename}
+    "if(NOT EXISTS \"${CMAKE_BINARY_DIR}/install_manifest.txt\")
+        message(WARNING \"Cannot find install manifest: \\\"${CMAKE_BINARY_DIR}/install_manifest.txt\\\"\")
+        return()
     endif()
-endforeach(file)
-")
+    file(READ \"${CMAKE_BINARY_DIR}/install_manifest.txt\" files)
+    string(STRIP \"\${files}\" files)
+    string(REGEX REPLACE \"\\n\" \";\" files \"\${files}\")
+    list(REVERSE files)
+    foreach(file \${files})
+        message(STATUS \"Uninstalling: \$ENV{DESTDIR}\${file}\")
+        if(EXISTS \"\$ENV{DESTDIR}\${file}\")
+            execute_process(
+                COMMAND \${CMAKE_COMMAND} -E remove \"\$ENV{DESTDIR}\${file}\"
+                OUTPUT_VARIABLE rm_out
+                RESULT_VARIABLE rm_retval)
+            if(NOT \"\${rm_retval}\" EQUAL 0)
+                message(FATAL_ERROR \"Problem when removing \\\"\$ENV{DESTDIR}\${file}\\\"\")
+            endif()
+        else()
+            message(STATUS \"File \\\"\$ENV{DESTDIR}\${file}\\\" does not exist.\")
+        endif()
+    endforeach(file)
+    ")
 
-add_custom_target(${_uninstall} COMMAND "${CMAKE_COMMAND}" -P "${_filename}")
-set_property(TARGET ${_uninstall} PROPERTY FOLDER "CMakePredefinedTargets")
+    add_custom_target(${_uninstall} COMMAND "${CMAKE_COMMAND}" -P "${_filename}")
+    set_property(TARGET ${_uninstall} PROPERTY FOLDER "CMakePredefinedTargets")
+endif()
